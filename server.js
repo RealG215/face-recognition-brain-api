013483cecca5d1a36080f53127d5a16db7514368
Ching Ho <edwardyuen0215@gmail.com>
2023-10-26 18:15:15 +0800

    your commit

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const db = knex({
   connection: {
     host : '127.0.0.1',
     user : 'postgres',
-    password : process.env.MY_PASS,
+    password : process.env.PASSWORD,
     database : 'smart-brain'
   }
 });
